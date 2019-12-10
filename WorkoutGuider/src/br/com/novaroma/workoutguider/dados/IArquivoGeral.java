ac0e45839ba14acaa95803ee19d75ac0642fc4a7
Anna Alice Guimarães <omfgitalice@gmail.com>
2019-12-10 00:12:13 -0300

    Ajuste de ícones e organização de código

--- WorkoutGuider/src/br/com/novaroma/workoutguider/dados/IArquivoGeral.java
+++ WorkoutGuider/src/br/com/novaroma/workoutguider/dados/IArquivoGeral.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 import br.com.novaroma.workoutguider.entidades.Usuario;
 
 public interface IArquivoGeral<T> {
-	
+
 	String gravarObjeto(T obj) throws ClassNotFoundException, IOException;
+
 	String gravaColecao(ArrayList<T> colecao) throws IOException;
+
 	ArrayList<T> retornaColecao() throws ClassNotFoundException, IOException;
+
 	String remover(int indice) throws ClassNotFoundException, IOException;
+
 	String alteraObjeto(int index, T objetoAlterado) throws ClassNotFoundException, IOException;
-	
+
 }
--- WorkoutGuider/src/br/com/novaroma/workoutguider/negocio/IArquivoGeral.java
+++ WorkoutGuider/src/br/com/novaroma/workoutguider/negocio/IArquivoGeral.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 import br.com.novaroma.workoutguider.entidades.Cliente;
 
 public interface IArquivoGeral<T extends Cliente> {
-	
+
 	String gravarObjeto(T obj) throws ClassNotFoundException, IOException;
+
 	String gravaColecao(ArrayList<T> colecao) throws IOException;
+
 	ArrayList<T> retornaColecao() throws ClassNotFoundException, IOException;
+
 	String remover(int indice) throws ClassNotFoundException, IOException;
+
 	String alteraObjeto(int index, T objetoAlterado) throws ClassNotFoundException, IOException;
 
 }
